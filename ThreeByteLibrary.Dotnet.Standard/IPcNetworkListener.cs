c55a2f85ee792bfca0d8fcb7676c5fc9535c7d92
Olaaf Rossi <olaaf.rossi@gmail.com>
2021-01-24 13:16:42 +0700

    removed the unused project

--- ThreeByteLibrary.Dotnet.Standard/IPcNetworkListener.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ThreeByteLibrary.Dotnet.Standard
-{
-    public interface IPcNetworkListener
-    {
-        int GetAppSettingsDataUdpPort();
-        void ListenLoop(object state);
-        void Run();
-    }
-}

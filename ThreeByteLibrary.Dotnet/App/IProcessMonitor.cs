8e8a6a6d3f2c7ea71bc7c2563df789dccdad02af
Olaaf Rossi <olaaf.rossi@gmail.com>
2021-03-16 22:15:25 +0700

    formatting fixes

---
+++ ThreeByteLibrary.Dotnet/App/IProcessMonitor.cs
@@ -0,0 +1,23 @@
+﻿// Created by Three Byte Intemedia, Inc. | project: PCController |
+// Created: 2021 03 16
+// by Olaaf Rossi
+
+using System;
+using System.Collections.Generic;
+
+namespace ThreeByteLibrary.Dotnet
+{
+    public interface IProcessMonitor
+    {
+        string ProcessName { get; }
+        string ExecutionString { get; }
+        int MaxResourceSnapshots { get; set; }
+        TimeSpan ResourceSnapshotInterval { get; set; }
+        TimeSpan UnresponsiveTimeout { get; set; }
+        event EventHandler<ProcessEventArgs> ProcessEvent;
+        event EventHandler ProcessExited;
+        IEnumerable<ResourceSnapshot> GetSnapshotHistory();
+        void Kill();
+        void Dispose();
+    }
+}
